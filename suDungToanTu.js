c7bc71a5215d9936941ad651db533233474709e9
Hong Son <dohongson27@gmail.com>
2024-04-16 18:54:16 +0700

    add file

---
+++ suDungToanTu.js
@@ -0,0 +1,6 @@
+inputWidth = prompt('Nhap chieu rong');
+inputHeight = prompt('Nhap chieu cao');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write('area=' + area);
